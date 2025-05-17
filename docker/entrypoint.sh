6a9eaae69aa6764b707b046bf408e26701f4ada1
kzy77 <kzy77@qq.com>
2025-05-17 19:20:58 +0800

    默认配置

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -11,25 +11,33 @@ cp ${TEMPLATE_FILE} ${CONFIG_FILE}
 # If an environment variable is not set, use an empty string or a default value if appropriate
 
 # VITE_GLOBAL_API
-if [ -n "${VITE_GLOBAL_API}" ]; then
-  sed -i "s#\\${VITE_GLOBAL_API}#${VITE_GLOBAL_API}#g" ${CONFIG_FILE}
+PLACEHOLDER_VITE_GLOBAL_API="\${VITE_GLOBAL_API}"
+DEFAULT_VITE_GLOBAL_API="https://scrawny-flss-chunkj-4938f5f4.koyeb.app"
+
+if [ -n "${VITE_GLOBAL_API}" ] && [ "${VITE_GLOBAL_API}" != "\${VITE_GLOBAL_API}" ]; then
+  sed -i "s#${PLACEHOLDER_VITE_GLOBAL_API}#${VITE_GLOBAL_API}#g" ${CONFIG_FILE}
 else
-  # Fallback to a default or leave as is if the template handles it
-  sed -i "s#\\${VITE_GLOBAL_API}#https://api-hot.efefee.cn#g" ${CONFIG_FILE} # Default example
+  sed -i "s#${PLACEHOLDER_VITE_GLOBAL_API}#${DEFAULT_VITE_GLOBAL_API}#g" ${CONFIG_FILE}
 fi
 
 # VITE_ICP
-if [ -n "${VITE_ICP}" ]; then
-  sed -i "s#\\${VITE_ICP}#${VITE_ICP}#g" ${CONFIG_FILE}
+PLACEHOLDER_VITE_ICP="\${VITE_ICP}"
+DEFAULT_VITE_ICP="豫ICP备2022018134号-1"
+
+if [ -n "${VITE_ICP}" ] && [ "${VITE_ICP}" != "\${VITE_ICP}" ]; then
+  sed -i "s#${PLACEHOLDER_VITE_ICP}#${VITE_ICP}#g" ${CONFIG_FILE}
 else
-  sed -i "s#\\${VITE_ICP}##g" ${CONFIG_FILE} # Default to empty if not set
+  sed -i "s#${PLACEHOLDER_VITE_ICP}#${DEFAULT_VITE_ICP}#g" ${CONFIG_FILE}
 fi
 
 # VITE_DIR
-if [ -n "${VITE_DIR}" ]; then
-  sed -i "s#\\${VITE_DIR}#${VITE_DIR}#g" ${CONFIG_FILE}
+PLACEHOLDER_VITE_DIR="\${VITE_DIR}"
+DEFAULT_VITE_DIR="/"
+
+if [ -n "${VITE_DIR}" ] && [ "${VITE_DIR}" != "\${VITE_DIR}" ]; then
+  sed -i "s#${PLACEHOLDER_VITE_DIR}#${VITE_DIR}#g" ${CONFIG_FILE}
 else
-  sed -i "s#\\${VITE_DIR}#/#g" ${CONFIG_FILE} # Default to '/' if not set
+  sed -i "s#${PLACEHOLDER_VITE_DIR}#${DEFAULT_VITE_DIR}#g" ${CONFIG_FILE}
 fi
 
 # Make sure the original template is not served by Nginx
